f07152feccc1d5f549b8f9ee512141b8424d7112
Tung Quan Hoang <hoangtungquan22@gmail.com>
2024-10-11 21:24:10 +1100

    add another test for staff

--- backend/src/tests/user_profile_info.test.ts
+++ backend/src/tests/user_profile_info.test.ts
@@ -47,7 +47,7 @@ describe('GET /user/profile_info', () => {
         userId: 1,
       };
 
-      const mockSessionToken = 'mockSessionToken';
+      const mockSessionToken = 'mockSessionTokenStudent';
 
       // Mocking the session repository's find method to return the userId
       mockSessionRepository.find.mockResolvedValue({
@@ -81,6 +81,57 @@ describe('GET /user/profile_info', () => {
       expect(result_2).toEqual(expect.objectContaining({
         name: 'Quan',
         email: 'hoangtungquan@gmail.com',
+        university: expect.any(String),
+      }));
+    });
+
+    test('staff success', async () => {
+      const mockStaff = {
+        name: 'Alice',
+        email: 'alice@example.com',
+        password: 'testPassword',
+        tshirtSize: 'M',
+        universityId: 1,
+      };
+
+      const mockUserId: UserIdObject = {
+        userId: 2,
+      };
+
+      const mockSessionToken = 'mockSessionTokenStaff';
+
+      // Mocking the session repository's find method to return the userId
+      mockSessionRepository.find.mockResolvedValue({
+        sessionId: mockSessionToken,
+        createdAt: new Date().getTime(),
+        userId: mockUserId.userId,
+      });
+
+      mockUserRepository.userProfileInfo.mockResolvedValue({
+        name: mockStaff.name,
+        email: mockStaff.email,
+        university: 'University of Melbourne', // TODO: find a way to not hardcode this
+      });
+
+      // Register the staff
+      mockUserRepository.staffRegister.mockResolvedValue(mockUserId);
+      await userService.staffRegister(mockStaff);
+
+      // Retrieve user profile info using the mock session token
+      const result = await userService.userProfileInfo(mockUserId.userId);
+
+      expect(result).toEqual(expect.objectContaining({
+        name: 'Alice',
+        email: 'alice@example.com',
+        university: expect.any(String), // You can also assert a specific university name if needed
+      }));
+
+      // Does not matter if we pass in another userId here, the mock will always return the same result
+      const result_2 = await userService.userProfileInfo(mockUserId.userId + 1);
+
+      expect(result_2).toEqual(expect.objectContaining({
+        name: 'Alice',
+        email: 'alice@example.com',
         university: expect.any(String),
       }));
     });
