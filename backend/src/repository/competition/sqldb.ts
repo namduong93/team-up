65355d27b200195c4ba60b509b1eeb1a158a427e
Julian Zincone <julianz4444@gmail.com>
2024-10-25 19:49:04 +1100

    tiny fix adding region to db query

--- backend/src/repository/competition/sqldb.ts
+++ backend/src/repository/competition/sqldb.ts
@@ -113,8 +113,8 @@ export class SqlDbCompetitionRepository implements CompetitionRepository {
     
     // Insert competition into competitions table
     const competitionQuery =
-    `INSERT INTO competitions (name, team_size, created_date, early_reg_deadline, general_reg_deadline, code, start_date)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    `INSERT INTO competitions (name, team_size, created_date, early_reg_deadline, general_reg_deadline, code, start_date, region)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
     RETURNING id, code;
     `;
     
@@ -125,7 +125,8 @@ export class SqlDbCompetitionRepository implements CompetitionRepository {
       new Date(competition.earlyRegDeadline),
       new Date(competition.generalRegDeadline),
       competition.code,
-      new Date(competition.startDate)
+      new Date(competition.startDate),
+      competition.region
     ];
     
     const competitionResult = await this.pool.query(competitionQuery, competitionValues);
