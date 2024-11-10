933d3fe48c63a20ba7fcd8bc85ab036072788b0c
Julian Zincone <julianz4444@gmail.com>
2024-11-10 22:51:29 +1100

    Removed default message at the bottom

--- chrome_extension/auto-coach/src/App.tsx
+++ chrome_extension/auto-coach/src/App.tsx
@@ -261,9 +261,6 @@ function App() {
           Submit Teams
         </StyledButton>
         </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
     </>
   )
