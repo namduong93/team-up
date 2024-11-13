126a14d93100c4d2dd5b8fba1e00249cd99efd38
Julian Zincone <julianz4444@gmail.com>
2024-11-13 11:24:58 +1100

    added teamlevel to the search keys in team display

--- frontend/src/screens/competition_staff_page/teams_page/TeamDisplay.tsx
+++ frontend/src/screens/competition_staff_page/teams_page/TeamDisplay.tsx
@@ -145,7 +145,7 @@ export const TeamDisplay: FC = () => {
   });
 
   const fuse = new Fuse(sortedTeamList, {
-    keys: ["teamName", "memberName1", "memberName2", "memberName3"],
+    keys: ["teamName", "memberName1", "memberName2", "memberName3", "teamLevel"],
     threshold: 0.5,
   });
 
