4cc51e7d5fdc3c1e1a9e6d469fc56d913743ccfb
Van Nam Duong <z5412692@ad.unsw.edu.au>
2024-11-18 16:56:01 +1100

    fix eslint

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -19,14 +19,20 @@ export default [
       reportUnusedDisableDirectives: true
     },
     rules: {
+      // TypeScript-specific rules
       '@typescript-eslint/explicit-member-accessibility': ['warn', {
         accessibility: 'no-public'
       }],
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
+
+      // Code readability and formatting
       'arrow-parens': ['warn', 'always'],
       'no-console': 'warn',
-      'no-debugger': 'warn'
+      'no-debugger': 'warn',
+      'indent': ['warn', 2, { SwitchCase: 1 }],
+      'quotes': ['warn', 'single', { avoidEscape: true }],
+      'semi': ['warn', 'always'],
     }
   }
 ];
